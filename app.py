eb11aaf0879180766c7ee508144f098078135260
SarveshModzYT <124548285+SarveshModzYT@users.noreply.github.com>
2023-02-11 21:18:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def hello_world():
    return 'LazyDeveloper'
 
-os.system("git clone https://LazyDeveloperr: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@github.com/LazyDeveloperr/Lazyv2testbot okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
+os.system("git clone https://LazyDeveloperr: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@github.com/SarveshModzYT/LazyPrincessV2 okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
